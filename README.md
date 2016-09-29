649b6b56d68e506da97705ad9241883143a7593a
andreepratama27 <andreepratama27@gmail.com>
2016-09-29 11:08:34 +0700

    INISIALISASI

---
+++ README.md
@@ -0,0 +1,17 @@
+# Install first ( there must be on your pc ):
+	* babel
+	* babel-cli
+*both install global*
+
+# Install dependencies such as : 
+	* webpack
+	* webpack-dev-server
+	* react
+	* react-dom
+*all of them install with the `--save` flags*
+
+# Install some babel packages : 
+	* babel-core
+	* babel-loader
+	* babel-preset-react
+	* babel-preset-es2015 
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var config = {
+    entry: __dirname + '/resources/assets/js/main.js',
+
+    output: {
+        output: __dirname + '/',
+        filename: 'index.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 3000
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
+
+module.exports = config;
